2978440bf3df027c44274c84d4b8b0a8e1204c5e
Patrick Quinn-Graham <make-contact@pftqg.com>
2014-07-01 19:22:11 -0700

    Initial Commit

---
+++ Standby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Standby
+//
+//  Created by Patrick Quinn-Graham on 1/07/2014.
+//  Copyright (c) 2014 Patrick Quinn-Graham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
